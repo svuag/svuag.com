16f2c02b964153643fbbc207726a1dd06401c4a4
Giulio Zanchetta <zank@outlook.it>
2021-02-25 22:19:07 +0100

    website refactor

---
+++ js/index.js
@@ -0,0 +1,16 @@
+/*
+Theme Name: HUGE
+Description: Creative Coming Soon Template
+Author: SquirrelLabs
+Author URI: https://themeforest.net/user/squirrellabs/portfolio?ref=SquirrelLab
+Version: 1.0
+License: https://themeforest.net/licenses/standard
+*/
+
+"use strict";
+
+$(window).on("load", function() {
+  $("#countdown").countdown($("#countdown").attr("data-time"), function(e) {
+      $(this).html(e.strftime("<div>%D<span>Days</span></div> <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>"))
+  });
+});
